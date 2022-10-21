21fb7a712c20fa2549da213e982ee7cc33eb49df
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-10-21 19:30:04 -0300

    Modificadas as Classes e Byte Code JAVA para a Seção 09 - Aula 05

--- src/com/udemy/java/Pessoa.java
+++ src/com/udemy/java/Pessoa.java
@@ -19,6 +19,16 @@ package com.udemy.java;
 //O nome não deve conter acentuação, caracteres especiais e espaçamento;
 //Os atributos podem ser de qualquer tipo de dado, incluindo arrays;
 
+//Aula 05 - Métodos
+
+//Podemos entender os métodos como a ação que é realizada por um objeto da classe;
+//Podemos entender também que os métodos são comportamentos dos objetos da classe;
+//Tipo de retorno (tipo de dado que o método vai retornar);
+//Nome correspondente a ação que o método realiza;
+//Parâmetros/Argumentos de entrada (opcional);
+//Retorno (opcional - depende do tipo de retorno);
+//Diferença entre Função e Método: o método depende de uma classe enquanto que a função não;
+
 
 public class Pessoa {
 	String nome, email;

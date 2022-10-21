fbb197779e5a2e536cf785dbde675c6669fad89f
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-10-21 19:04:24 -0300

    Adicionadas as Classes e Byte Code JAVA da Seção 09 - Aula 03

---
+++ src/com/udemy/java/Pessoa.java
@@ -0,0 +1,16 @@
+package com.udemy.java;
+
+//Seção 09 - Orientação a Objetos
+//Aula 03 - Classes
+
+//O nome das classes inicia com letra maiúscula;
+//O nome não deve conter acentuação, caracteres especiais e espaçamento
+//Nas classes Java, não existe a implementação da função main, pois a tornaria um programa e não uma classe;
+//Toda classe Java possui a seguinte forma: public class NomeDaClasse {}
+//O nome da classe Java deve ser o mesmo nome do arquivo Java;
+//Tudo o que estiver dentro das "{}" faz parte da classe;
+
+
+public class Pessoa {
+
+}
